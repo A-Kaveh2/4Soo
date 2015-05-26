50595208477f13a473b3de98e13982311e123e19
android1 <ab.kaveh@gmail.com>
2015-05-26 12:16:36 +0430

    I couldn't use the AppCompactActivity so I completely removed it from three activities.

--- app/src/main/java/ir/rasen/charsoo/view/widget_customized/charsoo_activity/NoActionBarActivity.java
+++ app/src/main/java/ir/rasen/charsoo/view/widget_customized/charsoo_activity/NoActionBarActivity.java
@@ -1,12 +1,13 @@
 package ir.rasen.charsoo.view.widget_customized.charsoo_activity;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBarActivity;
+
 
 /**
  * Created by Sina on 5/21/15.
  */
-public class NoActionBarActivity extends AppCompatActivity {
+public class NoActionBarActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/ir/rasen/charsoo/view/widget_customized/charsoo_activity/NoBackActivity.java
+++ app/src/main/java/ir/rasen/charsoo/view/widget_customized/charsoo_activity/NoBackActivity.java
@@ -5,7 +5,6 @@ import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 
@@ -15,7 +14,7 @@ import ir.rasen.charsoo.view.widget_customized.TextViewFontActionBarTitle;
 /**
  * Created by Sina on 5/21/15.
  */
-public class NoBackActivity extends AppCompatActivity {
+public class NoBackActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
